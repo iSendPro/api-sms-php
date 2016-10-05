3bacdf192613dcaa931a021c8a13161a8d89ddfe
iSendPro Telecom <anahit.dixdata@gmail.com>
2016-10-05 16:37:26 +0200

    Update testCampagne.php

--- testCampagne.php
+++ testCampagne.php
@@ -1,7 +1,8 @@
 <?php
+include("keyid.php");
 require_once("php-client/SwaggerClient-php/autoload.php");
 $api_instance = new Swagger\Client\Api\CampagneApi();
-$keyid = "d444279d6b9c10078b5416433b701414"; // string | Clee de compte iSendPro
+$keyid = $keyid; // string | Clee de compte iSendPro
 $rapport_campagne = "1"; // string |
 $date_deb = "2016-07-01 00:00"; // string | date de debut au format YYYY-MM-DD hh:mm
 $date_fin = "2016-07-13 23:00"; // string | date de fin au format YYYY-MM-DD hh:mm
